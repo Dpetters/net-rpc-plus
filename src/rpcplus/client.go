ee1bb2382c6d6bfa96d31e42ba1aa54c1a3e0230
Dmitrij Petters <Dpetters91@gmail.com>
2013-05-01 15:14:26 -0400

    updates

--- src/rpcplus/client.go
+++ src/rpcplus/client.go
@@ -1,35 +1,58 @@
 package rpcplus
 
 import (
+  "encoding/json"
 	"net/rpc"
-	"time"
-	"log"
+  "container/list"
+  "time"
+  "sync"
 )
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Structures
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 type RpcLog struct {
-	Blah string
+  mu sync.Mutex
+	rpcLogEntries list.List
+}
+
+type RpcLogEntry struct {
+  destinationAddress string
+  startTime time.Time
+  finishTime time.Time
+  args interface{}
+  reply interface{}
 }
 
 func MakeRpcLog(blah string) *RpcLog {
 	rpcLog := new(RpcLog)
-	rpcLog.Blah = blah
+	rpcLog.rpcLogEntries = *list.New()
 	return rpcLog
 }
 
-var theLog = MakeRpcLog("yo")
+var rpcLog = MakeRpcLog("yo")
 
-func Dial(network, address string) (*rpc.Client, error) {
-  log.Println("theLog.Blah", theLog.Blah)
-  //hmm open file each time?
-	//there is a new dial for each rpc.
-	return rpc.Dial(network, address)
-}
 
 func (client *rpc.Client) Call(serviceMethod string, args interface{}, reply interface{}) error {
-	log.Println("our Call")
-	start := time.Now()
+
+  if(rpcLog.rpcLogEntries.Len() == 100) {
+    rpcLog.mu.Lock()
+    jsonRpcLog, err := json.Marshal(rpcLog.rpcLogEntries)
+    rpcLog.rpcLogEntries = *list.New()
+    rpcLog.mu.Unlock()
+  }
+
+  address := client.request.RemoteAddr
+
+	startTime := time.Now()
 	err := client.Call(serviceMethod, args, reply)
-	elapsed := time.Since(start)
-  log.Printf("rpc took %v to run.\n", elapsed)
-	return err
+	finishTime := time.Now()
+
+  rpcLogEntry := RpcLogEntry{destinationAddress, startTime, finishTime, args}
+  rpc.LogEntry.pushFront(rpcLogEntry)
+
+  return err
 }
+
+
