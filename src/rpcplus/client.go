40c43b765a72d39abedf0b357e43c3895b588675
Brandon H Baker <bhbaker@mit.edu>
2013-05-01 23:44:16 -0400

    succesfully writing json to file

--- src/rpcplus/client.go
+++ src/rpcplus/client.go
@@ -1,14 +1,14 @@
 package rpcplus
 
 import (
-  //"encoding/json"
+  "encoding/json"
   "bufio"
-  //"os"
+  "os"
 	"net/rpc"
   "container/list"
   "time"
   "sync"
-  "runtime"
+	//"log"
 )
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -30,78 +30,83 @@ type RpcLog struct {
 }
 
 type RpcLogEntry struct {
-  sourceAddress string
-  destinationAddress string
-  startTime time.Time
-  finishTime time.Time
-  args interface{}
-  reply interface{}
+  SourceAddress string
+  DestinationAddress string
+  StartTime time.Time
+  FinishTime time.Time
+  //args interface{}
+  //reply interface{}
 }
 
 func MakeRpcLog(blah string) *RpcLog {
   rpcLog := new(RpcLog)
-  //rpcLog.rpcLogEntries = list.New()
+  rpcLog.rpcLogEntries = list.New()
 	return rpcLog
 }
 
-var rpcLog *RpcLog;//MakeRpcLog("yo")
+var rpcLog = MakeRpcLog("yo")
 
 
 func Dial(network, address string, me string) (*ClientPlus, error) {
    c, err := rpc.Dial(network, address)
 
-   if rpcLog == nil {
-     rpcLog = MakeRpcLog("yo")
-
-   }
-
-   if rpcLog.clientPlus == nil {
-     rpcLog.clientPlus = &ClientPlus{c, address}
-   }
-   /*
+   rpcLog.clientPlus = &ClientPlus{c, address}
+   
    rpcLog.me = me
    if rpcLog.w == nil {
      fo, err := os.Create("log.json")
      if err != nil { panic(err) }
      rpcLog.w = bufio.NewWriter(fo)
    }
-   */
+   
    return rpcLog.clientPlus, err
 }
 
 
-func (client *ClientPlus) Call(serviceMethod string, args interface{}, reply interface{}) error {
-  /*
-  if(rpcLog.rpcLogEntries.Len() == 100) {
-    rpcLog.mu.Lock()
+func (clientPlus *ClientPlus) Call(serviceMethod string, args interface{}, reply interface{}) error {
+  
+  //if(rpcLog.rpcLogEntries.Len() >= 100) {
+  //  rpcLog.mu.Lock()
 
-    buf, err := json.Marshal(rpcLog.rpcLogEntries)
+  //  buf, err := json.Marshal(rpcLog.rpcLogEntries)
+  //  if err != nil { panic(err) }
+	//	log.Printf("buf: %v\n", buf)
+
+  //  _, err2 := rpcLog.w.Write(buf);
+  //  if err2 != nil { panic(err2) }
+
+	//	err3 := rpcLog.w.Flush()
+	//	if err3 != nil { panic(err3) }
+
+  //  rpcLog.rpcLogEntries = list.New()
+  //  rpcLog.mu.Unlock()
+  //}
+  
+
+	startTime := time.Now()
+	err := clientPlus.client.Call(serviceMethod, args, reply)
+	finishTime := time.Now()
+
+  rpcLogEntry := RpcLogEntry{rpcLog.me, clientPlus.destinationAddress, startTime, finishTime}//, args, reply}
+  //rpcLog.rpcLogEntries.PushFront(rpcLogEntry)
+	
+	buf, err := json.Marshal(rpcLogEntry)
     if err != nil { panic(err) }
 
-    _, err2 := rpcLog.w.Write(buf);
-    if err2 != nil { panic(err2) }
+    rpcLog.w.Write(buf);
+    //if err2 != nil { panic(err2) }
 
-    rpcLog.rpcLogEntries = list.New()
-    rpcLog.mu.Unlock()
-  }
-  */
+		rpcLog.w.Flush()
+		//if err3 != nil { panic(err3) }
 
-	//startTime := time.Now()
-	err := client.Call(serviceMethod, args, reply)
-	//finishTime := time.Now()
-
-  //rpcLogEntry := RpcLogEntry{rpcLog.me, client.destinationAddress, startTime, finishTime, args, reply}
-  //rpcLogEntry = rpcLogEntry
-  //rpcLog.rpcLogEntries.PushFront(rpcLogEntry)
+    //rpcLog.rpcLogEntries = list.New()
 
   return err
 }
 
 
-func (client *ClientPlus) Close() error {
-  err := client.client.Close();
-  rpcLog.clientPlus = nil
-  runtime.GC()
+func (clientPlus *ClientPlus) Close() error {
+  err := clientPlus.client.Close();
   return err
 }
 
