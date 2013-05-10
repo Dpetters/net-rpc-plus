23c0cab7eb8a0be079525962355463f3292ea89c
Brandon H Baker <bhbaker@mit.edu>
2013-05-09 22:36:18 -0400

    fixed log json format issue.

--- server/src/rpcplus/client.go
+++ server/src/rpcplus/client.go
@@ -25,6 +25,7 @@ type RpcLog struct {
   clientPlus *ClientPlus
   name string
   w *bufio.Writer
+	first bool
 }
 
 type RpcLogEntry struct {
@@ -39,7 +40,7 @@ type RpcLogEntry struct {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var rpcLogs = make(map[string]RpcLog)
+var rpcLogs = make(map[string]*RpcLog)
 
 func Dial(myAddress string, network, address string) (*ClientPlus, error) {
    c, err := rpc.Dial(network, address)
@@ -66,9 +67,13 @@ func (clientPlus *ClientPlus) Call(myAddress string, serviceMethod string, args 
 
 		rpcLog.mu.Lock()
 		defer rpcLog.mu.Unlock()
+		if !rpcLog.first {
+			_, err2 = rpcLog.w.Write([]byte(",\n"))
+			if err2 != nil { log.Println("write err:", err2) }
+		} else {
+			rpcLog.first = false
+		}
 		_, err2 = rpcLog.w.Write(buf);
-		if err2 != nil { log.Println("write err:", err2) }
-		_, err2 = rpcLog.w.Write([]byte(",\n"))
 		if err2 != nil { log.Println("write err:", err2) }
 
 		return err
@@ -87,8 +92,9 @@ func (clientPlus *ClientPlus) Close(clusterName string) error {
 
 func SetupLogging(name string, logFilePath string, myAddress string) {
 	if _, ok := rpcLogs[myAddress]; !ok {
-		rpcLog := RpcLog{}
+		rpcLog := &RpcLog{}
 		rpcLog.name = name
+		rpcLog.first = true
 		fo, err := os.Create(logFilePath)
 		if err != nil { panic(err) }
 		_, err = io.WriteString(fo, "[")
@@ -101,7 +107,9 @@ func SetupLogging(name string, logFilePath string, myAddress string) {
 func Done(myAddress string) {
   rpcLog, ok := rpcLogs[myAddress]
 	if ok {
-		err := rpcLog.w.Flush()
+		_, err := rpcLog.w.Write([]byte("]"))
+		if err != nil { log.Println("write err:", err) }
+		err = rpcLog.w.Flush()
 		if err != nil { log.Println("flush failed:", err) }
 		delete(rpcLogs, myAddress)
 	}
